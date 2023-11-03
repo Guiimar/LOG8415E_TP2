f8da344d61ebc7821d1c53e95101efe97e61c690
MBelch <mehdi.belchiti@gmail.com>
2023-11-02 22:28:46 -0400

    changing the version of python in dockerfile

--- TP2/Setup/flask_workers.sh
+++ TP2/Setup/flask_workers.sh
@@ -63,12 +63,10 @@ EOL
 #create a dockerfile:
 cat <<EOL > /home/ubuntu/composeflask/Dockerfile
 # syntax=docker/dockerfile:1
-FROM python:3.8
-RUN pip install --upgrade pip
+FROM python:3.9
 WORKDIR /code
 ENV FLASK_APP=flask_app.py
 ENV FLASK_RUN_HOST=0.0.0.0
-RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 5000
