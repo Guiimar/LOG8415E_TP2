9a5ff27973d2aa2a8baa69ad4c9eab8d4935951e
Anass El Azouzi <anasselazouzimp2@gmail.com>
2023-11-05 17:41:26 -0500

    manage stored requests

--- TP2/Setup/orchestrator.py
+++ TP2/Setup/orchestrator.py
@@ -66,7 +66,7 @@ def process_request(incoming_request_data):
 def new_request():
     incoming_request_data=request.json
     # Si les requetes sont dans la queue, mettre une gestion FIFO des ancieenes & nouvelles requetes
-    if request_queue:
+    while request_queue:
         threading.Thread(target=process_request,args=(request_queue,)).start()
     threading.Thread(target=process_request,args=(incoming_request_data,)).start()
     return jsonify({"message":"Request received and processing started."})
