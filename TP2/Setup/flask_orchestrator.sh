589e8da9478e0244101072ac6e766b473ae8231d
MBelch <mehdi.belchiti@gmail.com>
2023-11-07 20:06:48 -0500

    Adding a feature in the sh file of the orchestrato

--- TP2/Setup/flask_orchestrator.sh
+++ TP2/Setup/flask_orchestrator.sh
@@ -48,7 +48,7 @@ lock=threading.Lock()
 request_queue=[]
 
 def send_request_to_container(container_id,container_info,incoming_request_data):
-    print(f"Sending request to{container_id} with data : {incoming_request_data}...")
+    print(f"Sending request to {container_id} with data : {incoming_request_data}...")
 
     container_ip=container_info["ip"]
     container_port=container_info["port"]
@@ -70,21 +70,21 @@ def send_request_to_container(container_id,container_info,incoming_request_data)
     else:
         print("La requête a échoué. Code d'état du serveur :", response.status_code)
 
-    print(f"Received response from{container_id}")
+    print(f"Received response from {container_id}")
 
 
 
 def update_container_status(container_id,status):
     with lock:
-        with open("/home/ubuntu/flaskapp/test.json","r") as f:
+        with open("test.json","r") as f:
             data=json.load(f)
         data[container_id]["status"]=status
-        with open ("/home/ubuntu/flaskapp/test.json","w") as f:
+        with open ("test.json","w") as f:
             json.dump(data,f)
 
 def process_request(incoming_request_data):
     with lock:
-        with open("/home/ubuntu/flaskapp/test.json","r") as f:
+        with open("test.json","r") as f:
             data=json.load(f)
     free_container=None
     for container_id, container_info in data.items():
@@ -96,24 +96,36 @@ def process_request(incoming_request_data):
         send_request_to_container(
             free_container,data[free_container],incoming_request_data
         )
-        #update_container_status(free_container,"free")
+        update_container_status(free_container,"free")
     else:
         #### Les requetes sont dans la queue QUE FAIRE POUR LES TRAITER
         request_queue.append(incoming_request_data)    
 
-@app.route("/new_request",methods=["POST"])
-def new_request():
+# @app.route("/new_request",methods=["POST"])
+# def new_request():
+#     incoming_request_data=""
+#     # Si les requetes sont dans la queue, mettre une gestion FIFO des ancieenes & nouvelles requetes
+#     while request_queue:
+#         threading.Thread(target=process_request,args=(request_queue,)).start()
+#     threading.Thread(target=process_request,args=(incoming_request_data,)).start()
+
+#     return jsonify({"message":"Request received and processing started."})
+
+if __name__ == '__main__':
+
     incoming_request_data=""
     # Si les requetes sont dans la queue, mettre une gestion FIFO des ancieenes & nouvelles requetes
     while request_queue:
         threading.Thread(target=process_request,args=(request_queue,)).start()
     threading.Thread(target=process_request,args=(incoming_request_data,)).start()
 
-    return jsonify({"message":"Request received and processing started."})
+    #return jsonify({"message":"Request received and processing started."})
 
 
 
 EOL
+
+#python /home/ubuntu/flaskapp/orchestrator.py
 
 #Install Gunicorn:
 
