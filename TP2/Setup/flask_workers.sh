e59b66b23524f851a98825b90f455257c0003c91
MBelch <mehdi.belchiti@gmail.com>
2023-11-11 18:15:26 -0500

    adding comments to flask_workers.sh

--- TP2/Setup/flask_workers.sh
+++ TP2/Setup/flask_workers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#Install Docker Engine on Ubuntu:
 sudo apt-get -y update
 sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+#Install Docker compose: 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
